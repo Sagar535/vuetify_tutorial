500d20e5f22676df1bb75da944eabb09253f9545
sagar <070bct535@ioe.edu.np>
2018-08-03 16:28:20 +0545

    error removed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <v-navigation-drawer
+  <v-app>
+    <v-toolbar class="deep-purple">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" dark></v-toolbar-side-icon>
+      <v-toolbar-title dark>Vuetify Tutorial</v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn flat dark>Vue</v-btn>
+        <v-btn flat dark>Vuex</v-btn>
+        <v-btn flat dark>Nuxt</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-navigation-drawer
       temporary
       v-model="drawer"
       :mini-variant="mini"
@@ -27,28 +40,15 @@
       </v-list-tile>
     </v-list>
     </v-navigation-drawer>
-  <v-app dark>
-    <v-toolbar class="light-blue">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" dark></v-toolbar-side-icon>
-      <v-toolbar-title>Vuetify Tutorial</v-toolbar-title>
-      <v-toolbar-items>
-        <v-btn flat dark>Vue</v-btn>
-        <v-btn flat dark>Vuex</v-btn>
-        <v-btn flat dark>Nuxt</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
   </v-app>
 </template>
 
 <script>
 export default {
-    data() {
-      return {
-        drawer: false
+  data() {
+    return {
+      drawer: false
     }
   }
-  }
+}
 </script>
