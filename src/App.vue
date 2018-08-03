9a383dc7240de79097c7cc955cfa8a4614231a70
sagar <070bct535@ioe.edu.np>
2018-08-03 16:27:08 +0545

    error generated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
+  <v-navigation-drawer
+      temporary
+      v-model="drawer"
+      :mini-variant="mini"
+      light
+      absolute
+    >
+      <v-toolbar flat class="transparent">
+      <v-list class="pa-0">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+    <v-list class="pt-0" dense>
+      <v-divider></v-divider>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon></v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+    </v-navigation-drawer>
   <v-app dark>
     <v-toolbar class="light-blue">
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" dark></v-toolbar-side-icon>
       <v-toolbar-title>Vuetify Tutorial</v-toolbar-title>
       <v-toolbar-items>
         <v-btn flat dark>Vue</v-btn>
@@ -16,6 +45,10 @@
 
 <script>
 export default {
-
+    data() {
+      return {
+        drawer: false
+    }
+  }
   }
 </script>
